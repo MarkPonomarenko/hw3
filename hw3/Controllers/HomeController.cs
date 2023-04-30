ce781d786ea7e5065ab6f15015c588b2e690704c
Марк Пономаренко <ponomarenko.marke@gmail.com>
2023-04-30 20:12:23 +0300

    solution init

---
+++ hw3/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using hw3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace hw3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            return View("Personal", user);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
